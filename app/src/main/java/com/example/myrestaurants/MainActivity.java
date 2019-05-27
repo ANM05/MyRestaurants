6eff5e45be594fdbb65b221db0f86c9412ca0341
Antony Nganga <antonynganga05@gmail.com>
2019-05-27 16:41:40 +0300

    Solve AppCompat issue

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myrestaurants;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.graphics.Typeface;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
